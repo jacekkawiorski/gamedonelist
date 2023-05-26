850091fc959ada254267c669febfc068693a0e08
jacekkawiorski <jkawiorski89@gmail.com>
2023-05-26 18:47:10 +0200

    Add error  handling

--- lib/app/login/login_page.dart
+++ lib/app/login/login_page.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   LoginPage({
     super.key,
   });
@@ -10,33 +10,47 @@ class LoginPage extends StatelessWidget {
   final passwordController = TextEditingController();
 
   @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  var errorMessage = '';
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
       child: Padding(
-        padding: EdgeInsets.all(20),
+        padding: const EdgeInsets.all(20),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const Text("Zaloguj się"),
             TextField(
-              controller: emailController,
+              controller: widget.emailController,
               decoration: const InputDecoration(hintText: 'Podaj E-mail'),
             ),
             TextField(
-              controller: passwordController,
+              controller: widget.passwordController,
               obscureText: true,
               decoration: const InputDecoration(hintText: 'Podaj Hasło'),
+            ),
+            const SizedBox(height: 35),
+            Text(
+              errorMessage,
+              style: const TextStyle(color: Colors.red),
             ),
             const SizedBox(height: 35),
             ElevatedButton(
               onPressed: () async {
                 try {
                   await FirebaseAuth.instance.signInWithEmailAndPassword(
-                      email: emailController.text,
-                      password: passwordController.text);
+                      email: widget.emailController.text,
+                      password: widget.passwordController.text);
                 } catch (error) {
-                  print(error);
+                  setState(() {
+                    errorMessage = error.toString();
+                  });
                 }
               },
               child: const Text('Zaloguj'),
