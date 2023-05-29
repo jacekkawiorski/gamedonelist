9b3736602349ebf578a98bded99742ec61540aaa
jacekkawiorski <jkawiorski89@gmail.com>
2023-05-29 20:08:18 +0200

    Implement add game

--- lib/app/home/add_game/add_game_page_content.dart
+++ lib/app/home/add_game/add_game_page_content.dart
@@ -1,14 +1,64 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class AddGamePageContent extends StatelessWidget {
+class AddGamePageContent extends StatefulWidget {
   const AddGamePageContent({
     super.key,
   });
 
   @override
+  State<AddGamePageContent> createState() => _AddGamePageContentState();
+}
+
+class _AddGamePageContentState extends State<AddGamePageContent> {
+  var gameName = '';
+  var system = '';
+  var playTime = '';
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Dwa'),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextField(
+            decoration: const InputDecoration(hintText: 'Podaj nazwę gry'),
+            onChanged: (newValue) {
+              setState(() {
+                gameName = newValue;
+              });
+            },
+          ),
+          TextField(
+            decoration: const InputDecoration(hintText: 'Podaj nazwe systemu'),
+            onChanged: (newValue) {
+              setState(() {
+                system = newValue;
+              });
+            },
+          ),
+          TextField(
+            decoration: const InputDecoration(hintText: 'Podaj czas przejscia'),
+            keyboardType: TextInputType.number,
+            onChanged: (newValue) {
+              setState(() {
+                playTime = newValue;
+              });
+            },
+          ),
+          const SizedBox(height: 50),
+          ElevatedButton(
+            onPressed: () {
+              FirebaseFirestore.instance.collection('games').add({
+                'gameName': gameName,
+                'system': system,
+                'time': playTime,
+                'rating': 4.5,
+              });
+            },
+            child: const Text('Dodaj do listy'),
+          )
+        ],
+      ),
     );
   }
 }
